65ce803f00f8a0a48d8e314d173c487af20ce6f7
Abdulbasit110 <BASIT4408944@cloud.neduet.edu.pk>
2024-08-19 18:56:22 +0500

    edited contacts page theme

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -22,9 +22,7 @@ function Contact() {
       <h3 className="text-center text-xl md:text-4xl lg:text-6xl text-gradient font-semibold md:font-bold pt-5 md:pt-10 md:pb-6">
         <a href={`mailto:${email2}`}>{email2}</a>
       </h3>
-      <span className="text-center text-content text-xl font-light block">
-        or
-      </span>
+      <span className="text-center text-xl font-light block">or</span>
       <h3 className="text-center text-3xl md:text-4xl lg:text-6xl text-gradient font-semibold md:font-bold pt-2 md:py-6">
         <a href={`tel:${phone}`}>{phone}</a>
       </h3>
